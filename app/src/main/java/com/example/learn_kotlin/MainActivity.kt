8c517969f87859177ff6b7003c471efaa9114433
roiskhoiron <rois.khoiron@gmail.com>
2019-08-08 13:31:05 +0700

    added fun to start the animate

--- app/src/main/java/com/example/learn_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/learn_kotlin/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.example.learn_kotlin
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.View.VISIBLE
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        object : CountDownTimer(5000,1000) {
+            override fun onFinish() {
+                JTransTextView.visibility = View.GONE
+                loadingProgressar.visibility = View.GONE
+                rootView.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.colorSplashText))
+                JTransIconImageView.setImageResource(R.drawable.logo_jtrans)
+                startAnimation()
+            }
+
+            override fun onTick(p0: Long) {}
+        }.start()
+    }
+
+    private fun startAnimation() {
+        JTransIconImageView.animate().apply {
+            x(50f)
+            y(100f)
+            duration = 1000
+        }.setListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                afterAnimationView.visibility = VISIBLE
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+
+            }
+        })
     }
 }
+
